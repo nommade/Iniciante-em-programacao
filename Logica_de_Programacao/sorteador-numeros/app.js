e39eed5e18209cc6cfd8477863c66ddc3be52c7e
Nomade do Vale Boreal <nomadedovaleboreal@gmail.com>
2025-01-31 20:39:19 -0300

    finalização do execicio sorteador de numeros

---
+++ Logica_de_Programacao/sorteador-numeros/app.js
@@ -0,0 +1,46 @@
+function gerarNumero(min, max) {
+    return Math.floor(Math.random()*(max-min+1))+min;
+}
+
+function alteraStatus() {
+    let btnReiniciar = document.getElementById("btn-reiniciar");
+
+    if(btnReiniciar.classList.contains("container__botao-desabilitado")) {
+        btnReiniciar.classList.remove("container__botao-desabilitado");
+        btnReiniciar.classList.add("container__botao");
+    } else {
+        btnReiniciar.classList.remove("container__botao");
+        btnReiniciar.classList.add("container__botao-desabilitado");
+    }
+}
+
+function sortear() {
+    let quantidade = parseInt(document.getElementById("quantidade").value);
+    let de = parseInt(document.getElementById("de").value);
+    let ate = parseInt(document.getElementById("ate").value);  
+    let resultado = document.getElementById("resultado");
+    
+    let sorteados = [];
+    let numero;
+
+    for(let i = 0; i < quantidade; i++) {
+        numero = gerarNumero(de, ate);
+        while(sorteados.includes(numero)){
+            numero = gerarNumero(de, ate);
+        }
+        sorteados.push(numero);
+    }
+
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados} </label>`;
+
+    alteraStatus();
+}
+
+function reiniciar() {
+    quantidade = document.getElementById("quantidade").value = "";
+    de = document.getElementById("de").value = "";
+    ate = document.getElementById("ate").value = "";
+    resultado = document.getElementById("resultado").innerHTML = `<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>`;
+
+    alteraStatus();
+}
