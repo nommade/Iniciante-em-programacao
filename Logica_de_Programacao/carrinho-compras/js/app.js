ec44d34fa58ffa11e7cd2b371977bc64d3a0db4e
Nomade do Vale Boreal <nomadedovaleboreal@gmail.com>
2025-02-02 16:10:02 -0300

    finalização do exercício carrinho de compras

---
+++ Logica_de_Programacao/carrinho-compras/js/app.js
@@ -0,0 +1,28 @@
+let totalGeral = 0;
+limpar();
+
+function adicionar() {
+    let selecao = document.getElementById("produto").value;
+    let produtoNome = selecao.split(`-`)[0];
+    let produtoPreco = selecao.split(`R$`)[1];
+    let quantidade = document.getElementById("quantidade").value;
+    
+    let carrinho = document.getElementById("lista-produtos");
+    carrinho.innerHTML = carrinho.innerHTML + 
+            `<section class="carrinho__produtos__produto">
+                <span class="texto-azul">${quantidade}x</span> ${produtoNome} <span class="texto-azul">${produtoPreco}</span>
+            </section>`;
+
+    let preco = quantidade*parseInt(produtoPreco);
+    let subTotal = document.getElementById("valor-total");
+    totalGeral = totalGeral + preco;
+    subTotal.textContent = `R$ ${totalGeral}`;
+
+    quantidade = document.getElementById("quantidade").value = '';
+}
+
+function limpar() {
+    document.getElementById("lista-produtos").innerHTML = "";
+    document.getElementById("valor-total").innerHTML = "";
+    totalGeral = 0;
+}
